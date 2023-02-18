3e7f58ee52111c951d173ff49c293b519061bbca
Milan Gruner <milangruner@gmail.com>
2023-02-18 20:12:52 +0100

    Add steps and edit button to migration detail page

--- app/routes/migrations/$migrationId.tsx
+++ app/routes/migrations/$migrationId.tsx
@@ -1,10 +1,11 @@
 import type { ActionArgs, LoaderArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useCatch, useLoaderData } from "@remix-run/react";
+import { Form, Link, useCatch, useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
 
 import { deleteMigration, getMigration } from "~/models/migration.server";
 import { requireUserId } from "~/session.server";
+import { formatDate, useUser } from "~/utils";
 
 export async function loader({ request, params }: LoaderArgs) {
   const userId = await requireUserId(request);
@@ -28,16 +29,38 @@ export async function action({ request, params }: ActionArgs) {
 
 export default function MigrationDetailsPage() {
   const data = useLoaderData<typeof loader>();
+  const user = useUser();
 
   return (
     <div>
       <h3 className="text-2xl font-bold">{data.migration.title}</h3>
       <p className="py-6">{data.migration.description}</p>
+      {data.migration.imageUrl ?
+        <img src={data.migration.imageUrl} alt={data.migration.title} className="rounded-lg" />
+        : ""
+      }
+      <h4 className="text-xl font-bold my-3">Migration steps</h4>
+      {data.migration.steps.length === 0 ? <p>No steps yet</p> : (
+        <ul className="bg-white rounded-lg border border-gray-200 w-96 text-gray-900">
+          {data.migration.steps.map((step, i) => (
+            <li key={step.id} className={`px-6 py-2 border-b border-gray-200 w-full ${i === 0 ? 'rounded-t-lg' : ''} ${i === data.migration.steps.length - 1 ? 'rounded-b-lg' : ''}`}>
+              At {step.place.title} from {formatDate(step.startDate)} until {formatDate(step.endDate)}
+            </li>
+          ))}
+        </ul>
+      )}
+      {user.role === 'BIOLOGIST' ? (
+        <div className="my-6">
+          <Link to='./edit' className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400">
+            Add new steps
+          </Link>
+        </div>
+      ): ''}
       <hr className="my-4" />
       <Form method="post">
         <button
           type="submit"
-          className="rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+          className="rounded bg-red-500 py-2 px-4 text-white hover:bg-red-600 focus:bg-red-400"
         >
           Delete
         </button>
