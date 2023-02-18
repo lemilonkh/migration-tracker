c078c096b72b06549e0e1bc9f463e379251d24d1
Milan Gruner <milangruner@gmail.com>
2023-02-19 00:44:10 +0100

    Install package react-tailwindcss-datepicker

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  content: ["./app/**/*.{ts,tsx,jsx,js}", "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js"],
   theme: {
     extend: {},
   },
