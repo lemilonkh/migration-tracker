6c2870995b42e75e581343149a8c9f168f7e5a8f
Milan Gruner <milangruner@gmail.com>
2023-02-20 22:31:09 +0100

    Add function getPlace to place model

--- app/models/place.server.ts
+++ app/models/place.server.ts
@@ -1,7 +1,22 @@
 import { prisma } from "~/db.server";
+import type { Place } from "@prisma/client";
 
 export function getPlaces() {
   return prisma.place.findMany({
     orderBy: { title: "asc" },
+  });
+}
+
+export function getPlace(id: Place["id"]) {
+  return prisma.place.findFirst({
+    where: { id },
+    include: {
+      migrationSteps: {
+        include: { migration: true },
+        orderBy: {
+          startDate: 'asc',
+        }
+      },
+    },
   });
 }
