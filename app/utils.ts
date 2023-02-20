efa49cfaacc85e32073444c81831b5b3a3da9cc2
Milan Gruner <milangruner@gmail.com>
2023-02-20 21:05:51 +0100

    Implement validation helper isIsoDate

--- app/utils.ts
+++ app/utils.ts
@@ -84,3 +84,9 @@ export function formatDate(date: Date | string) {
     return dateFormat.format(date);
   }
 }
+
+export function isIsoDate(str: string) {
+  if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(str)) return false;
+  const d = new Date(str);
+  return d instanceof Date && d.toISOString()===str; // valid date
+}
