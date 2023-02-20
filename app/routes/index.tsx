c323a72ec0785c0c1b0935d7ade2d0efa3973ad3
Milan Gruner <milangruner@gmail.com>
2023-02-21 00:44:54 +0100

    Add direct link to view migrations when not logged in on landing page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -35,7 +35,13 @@ export default function Index() {
                     View migrations
                   </Link>
                 ) : (
-                  <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+                  <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-3 sm:gap-5 sm:space-y-0">
+                    <Link
+                      to="/migrations"
+                      className="flex items-center justify-center rounded-md bg-green-500 px-4 py-3 font-medium text-white hover:bg-green-600"
+                    >
+                      View migrations
+                    </Link>
                     <Link
                       to="/join"
                       className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-green-700 shadow-sm hover:bg-green-50 sm:px-8"
@@ -44,7 +50,7 @@ export default function Index() {
                     </Link>
                     <Link
                       to="/login"
-                      className="flex items-center justify-center rounded-md bg-green-500 px-4 py-3 font-medium text-white hover:bg-green-600"
+                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-green-700 shadow-sm hover:bg-green-50 sm:px-8"
                     >
                       Log In
                     </Link>
